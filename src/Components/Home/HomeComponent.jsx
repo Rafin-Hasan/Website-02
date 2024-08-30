05351d6870d9f1a8368ca4329ca6b3fd317b604a
Rafin Hasan <168470414+Rafin-Hasan@users.noreply.github.com>
2024-08-30 08:02:45 +0600

    new file:   public/img/icn1.png 	new file:   public/img/icn2.png 	new file:   public/img/icn3.png 	new file:   public/img/icn4.png 	new file:   public/img/qute.png 	modified:   src/Components/Home/HomeComponent.jsx 	modified:   src/Components/Seo/SeoComponent.jsx

--- src/Components/Home/HomeComponent.jsx
+++ src/Components/Home/HomeComponent.jsx
@@ -62,17 +62,88 @@ const HomeComponent = () => {
               scalable.
             </p>
           </div>
-          <div className="" data-aos="fade-left" data-aos-duration="1000">
-            <div className="flex justify-between gap-[77px]">
-              <img src="./img/one.png" alt="True Cloud Pic" />
-              <img src="./img/two.png" alt="True Cloud Pic" />
-              <img src="./img/three.png" alt="True Cloud Pic" />
-            </div>
-            <div className="flex justify-between pt-[55.5px]">
-              <img src="./img/four.png" alt="True Cloud Pic" />
-              <img src="./img/five.png" alt="True Cloud Pic" />
-              <img src="./img/six.png" alt="True Cloud Pic" />
-            </div>
+          <div
+            className="flex flex-wrap justify-between gap-[65px] pl-[93px]"
+            data-aos="fade-left"
+            data-aos-duration="1000"
+          >
+            <img
+              className="w-[140px] h-[30px]"
+              src="./img/one.png"
+              alt="True Cloud Pic"
+            />
+            <img
+              className="w-[125px] h-[50px]"
+              src="./img/two.png"
+              alt="True Cloud Pic"
+            />
+            <img
+              className="w-[125px] h-[53px]"
+              src="./img/three.png"
+              alt="True Cloud Pic"
+            />
+
+            <img
+              className="w-[172px] h-[92px]"
+              src="./img/four.png"
+              alt="True Cloud Pic"
+            />
+            <img
+              className="w-[168px] h-[56px]"
+              src="./img/five.png"
+              alt="True Cloud Pic"
+            />
+            <img
+              className="w-[143px] h-[47px]"
+              src="./img/six.png"
+              alt="True Cloud Pic"
+            />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div className="text-center w-[252px]" data-aos="fade-down-right">
+            <img className="m-auto" src="./img/icn1.png" alt="clock" />
+            <h5 className="header font-bold text-[20px] pt-3 pb-1">
+              99.9% Uptime
+            </h5>
+            <p className="header font-light text-[12px]">
+              We Keep Your Web build Online 24x7x365. Downtime not only costs
+              you lost visitors but also damages your reputation and search
+              engine rankings.
+            </p>
+          </div>
+          <div className="text-center w-[252px]" data-aos="zoom-in">
+            <img className="m-auto" src="./img/icn2.png" alt="clock" />
+            <h5 className="header font-bold text-[20px] pt-3 pb-1">
+              Blazing Fast Web Hosting{" "}
+            </h5>
+            <p className="header font-light text-[12px]">
+              We Keep Your Web build Online 24x7x365. Downtime not only costs
+              you lost visitors but also damages your reputation and search
+              engine rankings.
+            </p>
+          </div>
+          <div className="text-center w-[252px]" data-aos="zoom-in">
+            <img className="m-auto" src="./img/icn3.png" alt="clock" />
+            <h5 className="header font-bold text-[20px] pt-3 pb-1">
+              Free SSL Certificates
+            </h5>
+            <p className="header font-light text-[12px]">
+              We Keep Your Web build Online 24x7x365. Downtime not only costs
+              you lost visitors but also damages your reputation and search
+              engine rankings.
+            </p>
+          </div>
+          <div className="text-center w-[252px]" data-aos="fade-down-left">
+            <img className="m-auto" src="./img/icn4.png" alt="clock" />
+            <h5 className="header font-bold text-[20px] pt-3 pb-1">
+              24x7 Friendly Support
+            </h5>
+            <p className="header font-light text-[12px]">
+              We Keep Your Web build Online 24x7x365. Downtime not only costs
+              you lost visitors but also damages your reputation and search
+              engine rankings.
+            </p>
           </div>
         </div>
       </section>
